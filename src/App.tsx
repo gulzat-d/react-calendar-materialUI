d232a6cba6b435fdb4b745681680ebd368cf1a0f
Gulzat <g.djanabaeva@mininform74.ru>
2024-03-15 14:22:18 +0500

    Инициализация проекта

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import EventCalendar from "./components/EventCalendar"
+import dayjs from 'dayjs';
+import weekday from 'dayjs/plugin/weekday';
+import 'dayjs/locale/ru';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
+
+dayjs.locale('ru');
+dayjs.extend(weekday);
+
+function App() {
+  return (
+    <>
+      <EventCalendar />
+    </>
+  )
+}
+
+export default App
